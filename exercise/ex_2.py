7075c56a66b397bb7e7ee9b84b6b95d2cf7fd2c0
Agata Rosada <75192@student.ue.poznan.pl>
2023-03-27 12:47:04 +0200

    Koło

--- exercise/ex_2.py
+++ exercise/ex_2.py
@@ -2,7 +2,7 @@
 #kwadrat
 a = 10
 obwod = a*4
-pole = a^2
+pole = a**2
 #Obwod kwadratu wynosi 40, a pole 100
 print("Obwod kwadratu wynosi " + str(obwod) + ", a pole " + str(pole) + ".")
 
@@ -12,3 +12,10 @@ b=10
 obwod=2*a+2*b
 pole=a*b
 print("Obwod prostokata wynosi "+str(obwod)+", a pole "+str(pole)+".")
+
+#kolo
+r=4
+obwod=2*3.14*r
+pole=3.14*r**2
+print("Obwod kola wynosi "+str(obwod)+", a pole "+str(pole)+".")
+
