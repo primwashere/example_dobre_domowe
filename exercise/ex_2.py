d53ec6ac66abeb229fa0955c3b6f4e6f6ad8bc70
Agata Rosada <75192@student.ue.poznan.pl>
2023-03-27 12:27:34 +0200

    Prostokat

--- exercise/ex_2.py
+++ exercise/ex_2.py
@@ -5,3 +5,10 @@ obwod = a*4
 pole = a^2
 #Obwod kwadratu wynosi 40, a pole 100
 print("Obwod kwadratu wynosi " + str(obwod) + ", a pole " + str(pole) + ".")
+
+#prostokat
+a=5
+b=10
+obwod=2*a+2*b
+pole=a*b
+print("Obwod prostokata wynosi "+str(obwod)+", a pole "+str(pole)+".")
