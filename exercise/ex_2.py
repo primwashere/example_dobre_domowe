21940f2c19a17bd1e51a549133ec3c99cb16571f
Agata Rosada <75192@student.ue.poznan.pl>
2023-03-27 12:18:29 +0200

    Kwadrat

---
+++ exercise/ex_2.py
@@ -0,0 +1,7 @@
+#tak sie robi komentarz
+#kwadrat
+a = 10
+obwod = a*4
+pole = a^2
+#Obwod kwadratu wynosi 40, a pole 100
+print("Obwod kwadratu wynosi " + str(obwod) + ", a pole " + str(pole) + ".")
